572307407dd31a6b88336915426ae23acaa60c81
Suriya Prakash S <126535913+suriya-prakash-s@users.noreply.github.com>
2026-05-21 08:40:17 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // Generates an optimized minimal server deployment
 };
 
 export default nextConfig;
